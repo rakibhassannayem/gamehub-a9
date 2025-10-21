44b2e5ab226248d7238adc491ebd0102196089ab
rakibhassannayem <rakibhassannayem@gmail.com>
2025-10-21 21:43:29 +0600

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { RouterProvider } from "react-router";
+import { router } from "./routes/router.jsx";
+import { ToastContainer } from "react-toastify";
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+    <ToastContainer autoClose={3000} />
+  </StrictMode>
+);
