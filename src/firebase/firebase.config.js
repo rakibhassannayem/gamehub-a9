30e2c77a445eff8c34d1de6ee844978cc6c6062f
rakibhassannayem <rakibhassannayem@gmail.com>
2025-10-22 13:52:34 +0600

    env

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDW6vG2fApk4OXUpj9svGA8vx3UjgQsNDg",
-  authDomain: "gamehub-7fab1.firebaseapp.com",
-  projectId: "gamehub-7fab1",
-  storageBucket: "gamehub-7fab1.firebasestorage.app",
-  messagingSenderId: "156437067807",
-  appId: "1:156437067807:web:53039c3481c873c6897d2a",
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId,
 };
 
 // Initialize Firebase
