b1cc7dbc4944f66e7dc37621eede1b339bf43643
rakibhassannayem <rakibhassannayem@gmail.com>
2025-10-23 08:28:25 +0600

    fix login register loading

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,6 @@
 import { use, useRef, useState } from "react";
 import { Link, useLocation, useNavigate } from "react-router";
 import { toast } from "react-toastify";
-import LoadingComp from "../components/LoadingComp";
 import { IoIosEye, IoIosEyeOff } from "react-icons/io";
 import { AuthContext } from "../provider/AuthContext";
 
@@ -9,7 +8,6 @@ const Login = () => {
   const {
     signInWithEmailAndPasswordFunc,
     signInWithPopupFunc,
-    loading,
     sendPasswordResetEmailFunc,
   } = use(AuthContext);
   const location = useLocation();
@@ -30,7 +28,6 @@ const Login = () => {
       })
       .catch((error) => {
         const errorCode = error.code;
-        // const errorMessage = error.message;
         setError(errorCode);
       });
   };
@@ -64,97 +61,91 @@ const Login = () => {
   return (
     <div>
       <title>Gamehub-login</title>
-      {loading ? (
-        <LoadingComp />
-      ) : (
-        <div className="text-white flex justify-center">
-          <div className="card bg-gray-800 rounded-xl w-full max-w-sm shrink-0 shadow-2xl pt-4">
-            <h2 className="font-semibold text-2xl text-center">
-              Login your account
-            </h2>
-            <form onSubmit={handleLogin} className="card-body">
-              <fieldset className="fieldset">
-                <label className="label text-lg font-semibold">Email</label>
+      <div className="text-white flex justify-center">
+        <div className="card bg-gray-800 rounded-xl w-full max-w-sm shrink-0 shadow-2xl pt-4">
+          <h2 className="font-semibold text-2xl text-center">
+            Login your account
+          </h2>
+          <form onSubmit={handleLogin} className="card-body">
+            <fieldset className="fieldset">
+              <label className="label text-lg font-semibold">Email</label>
+              <input
+                name="email"
+                type="email"
+                ref={emailRef}
+                className="input border"
+                placeholder="Enter your email"
+                required
+              />
+              <div className="relative">
+                <label className="label text-lg font-semibold">Password</label>
                 <input
-                  name="email"
-                  type="email"
-                  ref={emailRef}
+                  name="password"
+                  type={show ? "text" : "password"}
                   className="input border"
-                  placeholder="Enter your email"
+                  placeholder="write 6 digit password"
                   required
                 />
-                <div className="relative">
-                  <label className="label text-lg font-semibold">
-                    Password
-                  </label>
-                  <input
-                    name="password"
-                    type={show ? "text" : "password"}
-                    className="input border"
-                    placeholder="write 6 digit password"
-                    required
-                  />
-                  <span
-                    onClick={() => setShow(!show)}
-                    className="absolute right-7 bottom-3 cursor-pointer z-10"
-                  >
-                    {show ? <IoIosEyeOff size={18} /> : <IoIosEye size={18} />}
-                  </span>
-                </div>
-
-                {error && <p className="text-red-500">{error}</p>}
-
-                <div>
-                  <a
-                    onClick={handleForgotPass}
-                    className="link link-hover text-[14px]"
-                  >
-                    Forgot password?
-                  </a>
-                </div>
-
-                <button
-                  type="submit"
-                  className="btn border-0 rounded-lg bg-purple-800 mt-4 text-xl"
+                <span
+                  onClick={() => setShow(!show)}
+                  className="absolute right-7 bottom-3 cursor-pointer z-10"
                 >
-                  Login
-                </button>
+                  {show ? <IoIosEyeOff size={18} /> : <IoIosEye size={18} />}
+                </span>
+              </div>
 
-                {/* Divider */}
-                <div className="flex items-center justify-center gap-2 my-2">
-                  <div className="h-px w-16 bg-white/30"></div>
-                  <span className="text-sm text-white/70">or</span>
-                  <div className="h-px w-16 bg-white/30"></div>
-                </div>
+              {error && <p className="text-red-500 text-[16px]">{error}</p>}
 
-                {/* Google Signin */}
-                <button
-                  type="button"
-                  onClick={handleGoogleSignin}
-                  className="btn border-0 flex items-center justify-center gap-3 bg-purple-800 text-white text-[16px] px-5 py-2 rounded-lg w-full font-semibold cursor-pointer"
+              <div>
+                <a
+                  onClick={handleForgotPass}
+                  className="link link-hover text-[14px]"
                 >
-                  <img
-                    src="https://www.svgrepo.com/show/475656/google-color.svg"
-                    alt="google"
-                    className="w-5 h-5"
-                  />
-                  Continue with Google
-                </button>
+                  Forgot password?
+                </a>
+              </div>
 
-                <p className="mt-3 text-center font-semibold text-[14px]">
-                  Don't have an account?{" "}
-                  <Link
-                    to={"/auth/register"}
-                    className="text-purple-500! hover:underline"
-                  >
-                    Register
-                  </Link>
-                </p>
-              </fieldset>
-            </form>
-          </div>
+              <button
+                type="submit"
+                className="btn border-0 rounded-lg bg-purple-800 mt-4 text-xl"
+              >
+                Login
+              </button>
+
+              {/* Divider */}
+              <div className="flex items-center justify-center gap-2 my-2">
+                <div className="h-px w-16 bg-white/30"></div>
+                <span className="text-sm text-white/70">or</span>
+                <div className="h-px w-16 bg-white/30"></div>
+              </div>
+
+              {/* Google Signin */}
+              <button
+                type="button"
+                onClick={handleGoogleSignin}
+                className="btn border-0 flex items-center justify-center gap-3 bg-purple-800 text-white text-[16px] px-5 py-2 rounded-lg w-full font-semibold cursor-pointer"
+              >
+                <img
+                  src="https://www.svgrepo.com/show/475656/google-color.svg"
+                  alt="google"
+                  className="w-5 h-5"
+                />
+                Continue with Google
+              </button>
+
+              <p className="mt-3 text-center font-semibold text-[14px]">
+                Don't have an account?{" "}
+                <Link
+                  to={"/auth/register"}
+                  className="text-purple-500! hover:underline"
+                >
+                  Register
+                </Link>
+              </p>
+            </fieldset>
+          </form>
         </div>
-      )}
+      </div>
     </div>
   );
 };
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,12 +1,11 @@
 import { use, useState } from "react";
 import { Link, useNavigate } from "react-router";
 import { toast } from "react-toastify";
-import LoadingComp from "../components/LoadingComp";
 import { IoIosEye, IoIosEyeOff } from "react-icons/io";
 import { AuthContext } from "../provider/AuthContext";
 
 const Register = () => {
-  const { createUserWithEmailAndPasswordFunc, setUser, updateUser, loading } =
+  const { createUserWithEmailAndPasswordFunc, setUser, updateUser } =
     use(AuthContext);
   const [passError, setPassError] = useState("");
   const [show, setShow] = useState(false);
@@ -56,81 +55,72 @@ const Register = () => {
   return (
     <div>
       <title>Gamehub-Register</title>
-      {loading ? (
-        <LoadingComp />
-      ) : (
-        <div className="text-white flex justify-center my-8">
-          <div className="card bg-gray-800 rounded-xl w-full max-w-sm shrink-0 shadow-2xl pt-4">
-            <h2 className="font-semibold text-2xl text-center">
-              Register your account
-            </h2>
-            <form onSubmit={handleRegister} className="card-body">
-              <fieldset className="fieldset">
-                <label className="label text-lg font-semibold">Name</label>
+      <div className="text-white flex justify-center my-8">
+        <div className="card bg-gray-800 rounded-xl w-full max-w-sm shrink-0 shadow-2xl pt-4">
+          <h2 className="font-semibold text-2xl text-center">
+            Register your account
+          </h2>
+          <form onSubmit={handleRegister} className="card-body">
+            <fieldset className="fieldset">
+              <label className="label text-lg font-semibold">Name</label>
+              <input
+                name="name"
+                type="text"
+                className="input border"
+                placeholder="Enter your name"
+                required
+              />
+              <label className="label text-lg font-semibold">Photo URL</label>
+              <input
+                name="photoUrl"
+                type="text"
+                className="input border"
+                placeholder="Enter your photo url"
+                required
+              />
+              <label className="label text-lg font-semibold">Email</label>
+              <input
+                name="email"
+                type="email"
+                className="input border"
+                placeholder="Enter your email"
+                required
+              />
+              <div className="relative">
+                <label className="label text-lg font-semibold">Password</label>
                 <input
-                  name="name"
-                  type="text"
+                  name="password"
+                  type={show ? "text" : "password"}
                   className="input border"
-                  placeholder="Enter your name"
+                  placeholder="write 6 digit password"
                   required
                 />
-                <label className="label text-lg font-semibold">Photo URL</label>
-                <input
-                  name="photoUrl"
-                  type="text"
-                  className="input border"
-                  placeholder="Enter your photo url"
-                  required
-                />
-                <label className="label text-lg font-semibold">Email</label>
-                <input
-                  name="email"
-                  type="email"
-                  className="input border"
-                  placeholder="Enter your email"
-                  required
-                />
-                <div className="relative">
-                  <label className="label text-lg font-semibold">
-                    Password
-                  </label>
-                  <input
-                    name="password"
-                    type={show ? "text" : "password"}
-                    className="input border"
-                    placeholder="write 6 digit password"
-                    required
-                  />
-                  <span
-                    onClick={() => setShow(!show)}
-                    className="absolute right-7 bottom-3 cursor-pointer z-10"
-                  >
-                    {show ? <IoIosEyeOff size={18} /> : <IoIosEye size={18} />}
-                  </span>
-                </div>
-
-                {passError && <p className="text-red-500">{passError}</p>}
-
-                <button
-                  type="submit"
-                  className="btn text-xl bg-purple-800 mt-4"
+                <span
+                  onClick={() => setShow(!show)}
+                  className="absolute right-7 bottom-3 cursor-pointer z-10"
                 >
-                  Register
-                </button>
-                <p className="mt-3 text-center font-semibold">
-                  Already have an account?{" "}
-                  <Link
-                    to={"/auth/login"}
-                    className="text-purple-500! hover:underline"
-                  >
-                    Login
-                  </Link>
-                </p>
-              </fieldset>
-            </form>
-          </div>
+                  {show ? <IoIosEyeOff size={18} /> : <IoIosEye size={18} />}
+                </span>
+              </div>
+
+              {passError && <p className="text-red-500">{passError}</p>}
+
+              <button type="submit" className="btn text-xl bg-purple-800 mt-4">
+                Register
+              </button>
+              <p className="mt-3 text-center font-semibold">
+                Already have an account?{" "}
+                <Link
+                  to={"/auth/login"}
+                  className="text-purple-500! hover:underline"
+                >
+                  Login
+                </Link>
+              </p>
+            </fieldset>
+          </form>
         </div>
-      )}
+      </div>
     </div>
   );
 };
